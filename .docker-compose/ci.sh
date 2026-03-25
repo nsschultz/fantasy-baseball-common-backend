3dbc895af57d479c4d984b8ffe6a6e13fc210e70
Nick Schultz <nsschultz@gmail.com>
2026-03-25 04:40:46 +0000

    fix name

--- .docker-compose/ci.sh
+++ .docker-compose/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 EXIT_CODE=0
-docker compose -f .docker-compose/docker-compose-ci.yaml -p fantasy-baseball-common-backend up --build --exit-code-from api || EXIT_CODE=$?
+docker compose -f .docker-compose/docker-compose-ci.yaml -p fantasy-baseball-common-backend up --build --exit-code-from common-backend || EXIT_CODE=$?
 docker compose -f .docker-compose/docker-compose-ci.yaml -p fantasy-baseball-common-backend down
 exit $EXIT_CODE
